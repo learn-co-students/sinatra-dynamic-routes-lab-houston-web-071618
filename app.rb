a8a759bec99f9e251b1c1ed3de5330269fffc98f
RebGov <becci.govert@gmail.com>
2018-08-06 21:00:19 -0500

    Done.

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
-    answer = ''
+    answer = 'Operation unavailable. Please pick: add, subtract, multiply, or divide.'
     case params[:operation]
     when 'add'
       answer = (number1 + number2).to_s
